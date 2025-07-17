ff4a09c5214184da5918d5c152bef2bf78553ec6
Shodzilla <garcia147juanp147@gmail.com>
2025-07-17 19:16:34 -0400

    asdas
    asdasd

--- Views/Templates/footer.php
+++ Views/Templates/footer.php
@@ -4,9 +4,9 @@
     <div class="footer-section">
       <h3>¿Como podemos ayudarte?</h3>
       <ul>
-        <li><a href="#">Adopción</a></li>
+        <li><a href="/Views/adoptar/index.php">Adopción</a></li>
         <li><a href="#">Preguntas frecuentes</a></li>
-        <li><a href="#">Soporte</a></li>
+        <li><a href="contacto">Soporte</a></li>
       </ul>
     </div>
 
@@ -22,10 +22,10 @@
     <div class="footer-section">
       <h3>Redes Sociales</h3>
       <div class="social-icons">
-        <a href="#"><i class="fab fa-facebook-f"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
-        <a href="#"><i class="fab fa-twitter"></i></a>
-        <a href="#"><i class="fab fa-youtube"></i></a>
+        <a href="www.facebook.com"><i class="fab fa-facebook-f"></i></a>
+        <a href="www.instagram.com"><i class="fab fa-instagram"></i></a>
+        <a href="www.x.com"><i class="fab fa-twitter"></i></a>
+        <a href="www.youtube.com"><i class="fab fa-youtube"></i></a>
       </div>
     </div>
   </div>
